9e3fbee48a6be84923eabe8a0ff4d08c658d6f6b
Scott Cunningham <scott.cunningham@eagles.oc.edu>
2022-09-13 11:45:33 -0500

    Dev listview processes

---
+++ src/components/index.vue
@@ -0,0 +1,9 @@
+tutorialsRef.get().then(function(snapshot) {
+    vat tutorials = [];
+    snapshot.forEach(function(childSnapshot) {
+      var id = childSnapshot.id;
+      var data = childSnapshot.val();
+      // ...
+      tutorials.push({ id: id, title: data.title, description: data.description});
+    });
+  });
