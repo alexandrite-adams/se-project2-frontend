35d68b0dbc4827e66f8b6c48431ac3bbe5e2df4f
alexandrite-adams <a.adams@eagles.oc.edu>
2022-09-15 22:04:06 -0500

    Cant be on same port

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   devServer: {
     host: 'localhost',
-    port: 8080
+    port: 8081
   },
   publicPath: process.env.NODE_ENV === 'development' ? '/' : '/courses/',
   transpileDependencies: ['vuetify'],
