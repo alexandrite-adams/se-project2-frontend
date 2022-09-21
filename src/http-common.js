5a390f3e7f768aa530e286b115611823be09d6b9
alexandrite-adams <a.adams@eagles.oc.edu>
2022-09-21 10:40:33 -0500

    Im tired of this grandpa - fix common

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 var baseURL = "";
 if (process.env.NODE_ENV === "development") {
-  baseURL = "http://localhost:8081/";
+  baseURL = "http://localhost:3002/";
 } else {
   baseURL = "http://project2.eaglesoftwareteam.com";
 }
