f377216d0f63ab245abde6d16aacebf11c5ffe48
alexandrite-adams <a.adams@eagles.oc.edu>
2022-09-21 10:26:26 -0500

    took away a course-t2

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 var baseURL = "";
 if (process.env.NODE_ENV === "development") {
-  baseURL = "http://localhost/course-t2";
+  baseURL = "http://localhost/";
 } else {
   baseURL = "/course-t2/";
 }
