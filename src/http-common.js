c3e1857b5176d17d870cd8bd364fe1514b9c0090
alexandrite-adams <a.adams@eagles.oc.edu>
2022-09-21 10:24:07 -0500

    Change http-common to push to aws

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,13 @@
 import axios from "axios";
+var baseURL = "";
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://localhost/course-t2";
+} else {
+  baseURL = "/course-t2/";
+}
+
 export default axios.create({
-  baseURL: "http://localhost:3002",
+  baseURL: baseURL,
   headers: {
     "Content-type": "application/json"
   }
