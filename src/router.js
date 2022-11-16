716d936d96f83714659244179f7b4c9b41c52a18
Alexandria Adams <alexm.adams@yahoo.com>
2022-11-16 00:43:33 -0600

    trying again

--- src/router.js
+++ src/router.js
@@ -5,7 +5,8 @@ Vue.use(Router);
 const router = new Router({
   mode: "history",
   // mode: "hash",
-  base: process.env.NODE_ENV === "development" ? "/" : "/",
+  base:
+    process.env.NODE_ENV === "development" ? "/" : "/project3/2022/t2/login",
   routes: [
     {
       path: "/",
